dc9c2f0b48c3189cd8076b8b955999777e483838
muzieryueniao@gmail.com <851119LPsq851214>
2019-09-06 17:16:03 +0800

    change the number of participate

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ SESSION_CONFIGS = [
     {
         'name': 'BriberyGame',
         'display_name': "决策研究",
-        'num_demo_participants': 100,
+        'num_demo_participants': 120,
         'app_sequence': [
                          'introduction',
                          'briberyGame',
